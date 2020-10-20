f86a501659948cadc630527f0cd88926fdc7ae57
zhangtao-159 <1594737150@qq.com>
2020-10-20 08:14:22 +0800

    test
    asdasd

--- src/views/cartpage/Cart.vue
+++ src/views/cartpage/Cart.vue
@@ -1,13 +1,25 @@
 <template>
-  <h2>Cart</h2>
+  <div>
+    <div class="header"></div>
+    <cartime></cartime>
+  </div>
 </template>
 
 <script>
+import cartime from './carhead/cartime'
 export default {
-
+  components:{
+    cartime
+  }
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+.header
+  width 100%
+  height 0.44rem
+  box-sizing border-box
+  border-top 1px solid #e9e9e9 
+  border-bottom 1px solid #e9e9e9 
+  margin-bottom 0.15rem
 </style>
---
+++ src/views/cartpage/carhead/cartime.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="shell">
+    <div class="left"></div>
+    <div class="frame"></div>
+    <div class="right"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+.shell
+  width 100%
+  height 0.7rem
+  display flex
+  .left
+    flex 1
+  .right
+    flex 1
+  .frame
+    flex 23
+    height 100%
+    box-sizing border-box
+    border-radius 0.2rem
+    background #fbf4dc
+</style>
+
+
