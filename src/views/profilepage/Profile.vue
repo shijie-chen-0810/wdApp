bc7b65c5c18cb4dead963a72f31536ef77b3ac84
yuyu8989-tech <2847139715@qq.com>
2020-10-21 08:30:57 +0800

    2020-10-21

--- src/views/profilepage/Profile.vue
+++ src/views/profilepage/Profile.vue
@@ -1,13 +1,80 @@
 <template>
-  <h2>Profile</h2>
+  <div class="profile">
+    <div class="login">
+      <div class="login-top">
+        <div>
+          <img src="../../assets/images/profile/my_wandoulogotop.png" alt="">
+        </div>
+        <p>点击登录 / 注册</p>
+      </div>
+      <ul class="login-content">
+        <li>
+          <p>-</p>
+          <span>低现积分</span>
+        </li>
+        <li>
+          <p>-</p>
+          <span>优惠卷</span>
+        </li>
+        <li>
+          <p>-</p>
+          <span>公主说</span>
+        </li>
+      </ul>
+    </div>
+    <profileorder></profileorder>
+    <profileserve></profileserve>
+  </div>
 </template>
 
 <script>
+import profileorder from "./profilefeature/profileorder"
+import profileserve from "./profilefeature/profileserve"
 export default {
-
+  components:{
+    profileorder,
+    profileserve
+  }
 }
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.profile
+
+  display flex
+  flex-direction column
+  justify-content flex-start
+  margin .15rem
+  .login
+    height 1.8rem
+    background url('../../assets/images/profile/my_backgroundtop.png') no-repeat
+    background-size 100% 100%
+    color #ffffff
+    display flex
+    flex-direction column
+    justify-content space-between
+    .login-top
+      display flex
+      align-items center
+      padding-top .35rem
+      >div
+        margin-left .15rem
+        width .5rem
+        height .5rem
+        img 
+          width 100%
+          height 100%
+      >p
+        font-size .18rem
+        margin-left .2rem
+    .login-content
+      display flex
+      margin-bottom .1rem
+      >li
+        flex 1
+        display flex
+        flex-direction column
+        justify-content center
+        align-items center
 
 </style>
---
+++ src/views/profilepage/profilefeature/profileserve.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="profile-serve">
+    <h3 class="myserve">我的服务</h3>
+    <ul>
+      <li>
+        <a href="">
+          <div></div>
+          <span></span>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../../assets/stylus/border.styl'
+
+.profile-serve
+  padding-bottom .2rem
+  margin 0 0.05rem
+  display flex
+  flex-direction column
+  border_1px(0 0 1px 0)
+  >.myserve
+    color #191919
+    font-weight normal
+    font-size .2rem
+    margin-bottom .08rem
+  ul 
+    display flex
+    li 
+      width 25%
+      height 1rem
+      a 
+        width 100%
+        height 100%
+        display flex
+        flex-direction column
+        justify-content center
+        align-items center
+        >div
+          width .32rem
+          height .32rem
+          background url('../../../assets/images/profile/myserve/myserve1.png') no-repeat 0 0/100% 100%
+
+
+  
+</style>
