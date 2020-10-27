7601eeb388a970d1c4f3f673db1c7ba3ed7e87bb
YaToWei <70989327+YaToWei@users.noreply.github.com>
2020-10-27 22:46:36 +0800

    10-27第六次提交

--- src/components/guard.js
+++
@@ -1,7 +0,0 @@
-router.beforeEach((to, from, next) => {
-    if (to.matched.length ===0) {  //如果未匹配到路由
-      from.path? next({ path:from.path}) : next('/err');   //如果上级也未匹配到路由则跳转主页面，如果上级能匹配到则转上级路由
-    } else {
-      next();    //如果匹配到正确跳转
-    }
-});  
