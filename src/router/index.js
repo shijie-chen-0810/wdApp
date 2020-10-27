cb6589f73ab4aa84535f23f5408bf25cdc13e4bc
YaToWei <70989327+YaToWei@users.noreply.github.com>
2020-10-27 22:31:22 +0800

    10-27第四次提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+
 const Home =  () => import('views/home/homepage/Home')
 const HomeSortPage =  () => import('views/homesort/HomeSortPage')
 const HomeZong =  () => import('views/home/Home')
@@ -22,11 +23,13 @@ import Comingcommit from '../views/goodsorder/orderdetail/Comingcommit.vue'
 
 Vue.use(VueRouter)
 
+
+
 const routes = [
   {
     path: '/',
-    redirect: '/home',
     component:HomeZong,
+    redirect: '/home',
     children: [
       {
         path: 'home',
@@ -108,10 +111,29 @@ const routes = [
   }
 ]
 
+
+
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
 })
+
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.length ===0) {  
+//     from.path? next({ path:from.path}) : next('/err'); 
+//   } else {
+//     next()
+//   }
+// }) 
+
+// router.beforeEach((to, from, next) => {
+//   // 此处判断条件我有看到其他人用to.matched.length ===0进行判断， 具体的还有待进一步验证，大体的思路就是这样的
+//   if (to.fullPath === '/') { 
+//     from.path ? next({ path:from.path }) : next('/err');   
+//   } else {
+//       next(); //如果匹配到正确跳转
+//   }
+// });
 
 export default router
